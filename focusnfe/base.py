a0af032b6885b85c0820c974ac46138b02c750f3
jcarvalho <joao.carvalho@maestrus.com>
2019-11-12 19:54:45 -0300

    first commit

--- focusnfe/base.py
+++ focusnfe/base.py
@@ -2,27 +2,38 @@ import requests
 import json
 
 
-class NotazzException(Exception):
+class FocusNFEException(Exception):
     pass
 
 
-class NotazzBase(object):
+class FocusNFEBase(object):
 
     api_key = None
+    environment = None
 
-    PRD_URI = 'https://app.notazz.com/api/'
+    PRD_URI = 'https://api.focusnfe.com.br/'
+    DEV_URI = 'https://homologacao.focusnfe.com.br/v2/nfse'
+
+    ENV_PRODUCTION = 1
+    ENV_DEVELOPMENT = 2
 
     @property
     def base_uri(self):
-        return NotazzBase.PRD_URI
+        if self.environment == FocusNFEBase.ENV_PRODUCTION:
+            return FocusNFEBase.PRD_URI
+        elif self.environment == FocusNFEBase.ENV_DEVELOPMENT:
+            return FocusNFEBase.DEV_URI
+        else:
+            raise FocusNFEException('Programming Error: Development invalid or not set')
 
-    def __init__(self, api_key):
+    def __init__(self, api_key, environment):
         self.api_key = api_key
+        self.environment = environment
 
     @staticmethod
     def load_testing_env_variables():
         import os
-        from notazz.environment import environment
+        from focusnfe.environment import environment
         for key, value in environment:
             try:
                 os.environ.setdefault(key, value)
@@ -34,24 +45,26 @@ class NotazzBase(object):
         :param response:
         :return:
         """
-        if response.status_code:
-            r = response.json()
-            if 'codigoProcessamento' in r:
-                code = r.get('codigoProcessamento')
-                if str(code) != '000':
-                    desc = r.get('motivo')
-                    raise NotazzException('{code} - {desc}'.format(
-                        code=code,
-                        desc=desc,
-                    ))
-        elif response.status_code == 401:
-            raise NotazzException('Auth Error: 401 - API key missing')
+        r = response.json()
+        if response.status_code in [200, 201]:
+            return r
+        elif response.status_code == 400:
+            raise FocusNFEException('{0} - {1}'.format(r.get('codigo'), r.get('mensagem')))
         elif response.status_code == 403:
-            raise NotazzException('Teje Preso!')
+            raise FocusNFEException('{0} - {1}'.format(r.get('codigo'), r.get('mensagem')))
         elif response.status_code == 404:
-            raise NotazzException('Programming error: 404 - URI not found')
+            raise FocusNFEException('{0} - {1}'.format(r.get('codigo'), r.get('mensagem')))
+        elif response.status_code == 415:
+            raise FocusNFEException('415 - Midia Invalida. JSON Mal formado')
+        elif response.status_code == 422:
+            raise FocusNFEException('{0} - {1}'.format(r.get('codigo'), r.get('mensagem')))
+        elif response.status_code == 429:
+            raise FocusNFEException('429 - Excesso de requisicoes atingida. Whoa Cowboy!')
         elif response.status_code == 500:
-            raise NotazzException('500 -Something wrong with Notazz Server')
+            raise FocusNFEException('500 - Erro de Servidor')
+
+    def do_get_request(self, url, params):
+        pass
 
     def do_post_request(self, url, params=None):
         params = params if params else {}
@@ -62,3 +75,8 @@ class NotazzBase(object):
         self.process_errors(response)
         return response
 
+    def do_put_request(self, url, params):
+        pass
+
+    def do_delete_request(self, url, params):
+        pass
